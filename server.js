134d2a1d22b4edae36236bcda197ee55afc84c98
Ruben <ruben@manzanares.com>
2026-03-30 13:34:03 +0200

    Fix: Add Chromium dependencies for Railway/Nixpacks and optimize Puppeteer

--- server.js
+++ server.js
@@ -26,7 +26,13 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-extensions']
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--disable-extensions'
+        ]
     }
 });
 
